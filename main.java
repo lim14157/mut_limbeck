3c5c9d47d1f68db93abb0c596b63b70f25c8a94f
Philipp Limbeck <lim14157@spengergasse.at>
2013-05-16 17:25:56 +0200

    commit nr4

--- main.java
+++ main.java
@@ -2,27 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Uebung3 {
+public class Uebung4 {
 
+	/**
+	 * int main(int argc, char **argv) { char name[255];
+	 * printf("Enter your name: "); fgets(name, 255, stdin);
+	 * printf("length = %d\n", strlen(name)); name[strlen(name)-1] = '\0';
+	 * printf("Hello, %s!\n", name); return 0; }
+	 */
 	public static void main(String[] args) {
+		String name = null;
 
-		System.out.println("Enter your firstname : ");
-
+		System.out.println("Enter your name: ");
 		try {
-			BufferedReader bufferRead1 = new BufferedReader(
-					new InputStreamReader(System.in));
-			String s1 = bufferRead1.readLine();
-
-			System.out.println("Now enter you lastname : ");
-			BufferedReader bufferRead2 = new BufferedReader(
-					new InputStreamReader(System.in));
-			String s2 = bufferRead2.readLine();
-
-			System.out.println("Hello " + s1 + " " + s2);
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			name = bufferRead.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		System.out.println("length = " + name.length());
+		System.out.println("Hello " + name);
 	}
 
 }
