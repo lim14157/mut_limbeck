c3e43539bc3ea4f946c2b5557255addde458516f
Philipp Limbeck <lim14157@spengergasse.at>
2013-05-16 17:05:19 +0200

    hello this is my second commit

--- main.java
+++ main.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Uebung1 {
 
 	public static void main(String[] args){
 		System.out.println("Hello World");
+public class Uebung2 {
+
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		String name = null;
+		
+		System.out.println("Enter your name: ");
+		
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			name = console.readLine();
+		}catch (IOException e){ e.printStackTrace(); }
+		
+		System.out.println("Length " + name.length() + "\n");
+		System.out.println("Hallo " + name);
+		
+
 	}
 }
